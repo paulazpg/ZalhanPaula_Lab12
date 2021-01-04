e0700807cc09efc06517ec69281b70e930c99fac
Paula - Georgiana ZALHAN <Paula.ZALHAN@office.ubbcluj.ro>
2021-01-04 23:00:29 +0200

    Add project files.

---
+++ ZalhanPaula_Lab12/ZalhanPaula_Lab12/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZalhanPaula_Lab12.Data;
+
+namespace ZalhanPaula_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+            Database = new ShoppingListDatabase(new RestService());
+
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ZalhanPaula_Lab12/ZalhanPaula_Lab12/Data/ShoppingListDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ZalhanPaula_Lab12.Models;
+namespace ZalhanPaula_Lab12.Data
+{
+    public class ShoppingListDatabase
+    {
+        IRestService restService;
+
+        public ShoppingListDatabase(IRestService restService)
+        {
+            this.restService = restService;
+        }
+
+        public Task<List<ShopList>> GetShopListsAsync()
+        {
+            return restService.RefreshDataAsync();
+        }
+
+        public Task SaveShopListAsync(ShopList item, bool isNewItem = true)
+        {
+            return restService.SaveShopListAsync(item, isNewItem);
+        }
+        public Task DeleteShopListAsync(ShopList item)
+        {
+            return restService.DeleteShopListAsync(item.ID);
+        }
+    }
+}
+       
